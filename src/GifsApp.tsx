e12da7b2530e7f40c1e02a2d5faec541a36e5443
Leandro Martello <maidenleo2000@gmail.com>
2026-02-19 15:35:43 -0300

    Seccion 7 - custom Hook useGifs

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -4,37 +4,13 @@ import { SearchBar } from "./shared/components/SearchBar";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { GifList } from "./gifs/components/GifList";
-import { useState } from "react";
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
-import type { Gif } from "./gifs/interfaces/gif.interface";
+import { useGifs } from "./gifs/hooks/useGifs";
+// import { useState } from "react";
+// import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+// import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [gifs, setGifs] = useState<Gif[]>([]);
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-
-  const handleTermClicked = (term: string) => {
-    handleSearch(term);
-  };
-
-  const handleSearch = async (query: string = "") => {
-    //console.log({ query });
-    // Tarea:
-    // Implementar la función handleSearch que debe:
-    // Validar que el query no esté vacío
-    // Convertir el query a minúsculas y eliminar espacios en blanco
-    // Evitar búsquedas duplicadas verificando si el término ya existe en previousTerms ( si existe, no hacer nada )
-    // Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo, es decir no puede ser un arreglo de más de 8.
-    const queryClean = query.toLocaleLowerCase().trim();
-    if (queryClean.length > 0) {
-      if (!previousTerms.includes(queryClean)) {
-        if (previousTerms.length == 8) previousTerms.pop();
-        setPreviousTerms([queryClean, ...previousTerms]); // --> otra forma setPreviousTerms([queryClean, ...previousTerms].slice(0,7));
-      }
-    }
-    const gifs = await getGifsByQuery(queryClean);
-    console.log({ gifs });
-    setGifs(gifs);
-  };
+  const { gifs, previousTerms, handleTermClicked, handleSearch } = useGifs();
   return (
     <>
       {/* Header  */}
---
+++ src/gifs/hooks/useGifs.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import type { Gif } from "../interfaces/gif.interface";
+import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
+
+export const useGifs = () => {
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
+  const handleTermClicked = (term: string) => {
+    handleSearch(term);
+  };
+
+  const handleSearch = async (query: string = "") => {
+    //console.log({ query });
+    // Tarea:
+    // Implementar la función handleSearch que debe:
+    // Validar que el query no esté vacío
+    // Convertir el query a minúsculas y eliminar espacios en blanco
+    // Evitar búsquedas duplicadas verificando si el término ya existe en previousTerms ( si existe, no hacer nada )
+    // Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo, es decir no puede ser un arreglo de más de 8.
+    const queryClean = query.toLocaleLowerCase().trim();
+    if (queryClean.length > 0) {
+      if (!previousTerms.includes(queryClean)) {
+        if (previousTerms.length == 8) previousTerms.pop();
+        setPreviousTerms([queryClean, ...previousTerms]); // --> otra forma setPreviousTerms([queryClean, ...previousTerms].slice(0,7));
+      }
+    }
+    const gifs = await getGifsByQuery(queryClean);
+    console.log({ gifs });
+    setGifs(gifs);
+  };
+  return {
+    //Props
+    gifs,
+    previousTerms,
+
+    //Metodos
+    handleTermClicked,
+    handleSearch,
+  };
+};
